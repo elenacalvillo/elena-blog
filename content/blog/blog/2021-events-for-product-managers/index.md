09db32fd232816e1f15acbfb7f933ce9b702ff9a
Elena Calvillo <elenaca@outlook.com>
2021-03-22 18:28:07 +0000

    created new tags template

--- content/blog/blog/2021-events-for-product-managers/index.md
+++ content/blog/blog/2021-events-for-product-managers/index.md
@@ -2,11 +2,10 @@
 date: "2021-02-18"
 title: 2021 Events for Product Managers
 description: 2021 Conferences for product managers and product designers you cannot miss!
-tags: Product Management
+tags: ["Product Management", "Conferences", "Events"]
 ---
 
 There’s so much benefit of attending product events. It goes from start building your network, get advice from experts and find new job positions, get discounts on tools or services, and learn from other people experience!
-
 
 ### 1. Mind the Product
 
@@ -14,13 +13,11 @@ This is for all levels of product experience from around the world to explore th
 
 [Conferences - Mind the Product](http://www.mindtheproduct.com/conferences/)
 
-
 ### 2. ProductCon
 
 Organized by Product School, is the largest online product management conference in the world. You get discount on some tools, opportunity to win prizes and recordings after the event!
 
 [ProductCon: World's Largest Product Management Conference](http://productschool.com/productcon/)
-
 
 ### 3. Front: UX and Product
 
@@ -28,19 +25,16 @@ One of the top product conferences for UX designers and product managers. 16 tal
 
 [Front - The product conference for UX designers and product managers](http://www.frontutah.com/conference/)
 
-
 ### 4. Product Design Week
 
 Future London Academy let you meet the teams behind the most ground-breaking products to learn how they approach design and UX strategy, work with clients, and improve their services.
 
 [OUR PROGRAMMES AND COURSES](http://futurelondonacademy.co.uk/en/courses)
 
-
 ### 5. INDUSTRY Virtual
 
 One of the biggest and best-known conferences for software product managers. Includes interactive Q&A’s, exclusive podcast series, video networking, and more.
 
 [INDUSTRY: The Product Conference](http://www.industryconference.com/)
-
 
 Some of these events are accessible online because of the pandemic, so it’s easier now for all of us to attend and start learning more about product design and management!
---
+++ src/templates/category.js
@@ -0,0 +1,145 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const BlogPostTemplate = ({ data, location }) => {
+  const post = data.markdownRemark
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const { previous, next } = data
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <article
+        className="blog-post col-md-8 mx-auto"
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <header>
+          <h1 itemProp="headline">{post.frontmatter.title}</h1>
+          <p className="post-description">{post.frontmatter.description}</p>
+          <p className="post-date">{post.frontmatter.date}</p>
+          <div className="d-inline-flex">
+            <p>
+              <strong>SHARE</strong>
+            </p>
+            <div className="s9-widget-wrapper"></div>
+          </div>
+        </header>
+        <section
+          dangerouslySetInnerHTML={{ __html: post.html }}
+          itemProp="articleBody"
+        />
+        <hr />
+      </article>
+      <nav className="blog-post-nav">
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Link
+                to={previous.fields.slug}
+                rel="prev"
+                className="prev-post hvr-icon-back"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-arrow-left hvr-icon"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+                  />
+                </svg>{" "}
+                {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link
+                to={next.fields.slug}
+                rel="next"
+                className="next-post hvr-icon-forward"
+              >
+                {next.frontmatter.title}{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-arrow-right hvr-icon"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                  />
+                </svg>
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </Layout>
+  )
+}
+
+export default BlogPostTemplate
+
+export const pageQuery = graphql`
+  query BlogPostBySlug(
+    $id: String!
+    $previousPostId: String
+    $nextPostId: String
+  ) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    markdownRemark(id: { eq: $id }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        description
+      }
+    }
+    previous: markdownRemark(id: { eq: $previousPostId }) {
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+      }
+    }
+    next: markdownRemark(id: { eq: $nextPostId }) {
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+      }
+    }
+  }
+`
